a9dd162e49d8a64007364e727fc0dc0607ec075f
Aashiq Mohamed Baig <43927911+AashiqMd@users.noreply.github.com>
2026-07-03 17:50:49 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groupMap = defaultdict(list)
+        for string in strs:
+            sortedString = "".join(sorted(string))
+            groupMap[sortedString].append(string)
+        return list(groupMap.values())
+        
+        # res = []
+        # groupMap = {}
+        # idx = 0
+
+        # for i, string in enumerate(strs):
+        #     key = "".join(sorted(string))
+        #     if key not in groupMap:
+        #         groupMap[key] = idx
+        #         idx+=1
+        #         res.append([string])
+        #     else:
+        #         res[groupMap[key]].append(string)
+        
+        # return res
