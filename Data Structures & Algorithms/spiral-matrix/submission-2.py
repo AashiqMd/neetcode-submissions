0d7bb2cc02f8deb3573e3b6e18b50050515b2fc4
Aashiq Mohamed Baig <43927911+AashiqMd@users.noreply.github.com>
2026-07-04 18:18:59 -0400

    Add: spiral-matrix - submission-2

---
+++ Data Structures & Algorithms/spiral-matrix/submission-2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        ans = []
+        m, n = len(matrix), len(matrix[0])
+
+        top, bottom, left, right = 0, m-1, 0, n-1
+        
+        while left <=right and top <= bottom:
+            for j in range(left,right+1):
+                ans.append(matrix[top][j])
+            top+=1
+
+            for i in range(top, bottom+1):
+                ans.append(matrix[i][right])
+            right-=1
+
+            if top <= bottom:
+                for j in range(right,left-1,-1):
+                    ans.append(matrix[bottom][j])
+                bottom-=1
+
+            if left <= right:
+                for i in range(bottom, top-1, -1):
+                    ans.append(matrix[i][left])
+                left+=1
+
+        return ans
+
+        # Parse the matrix in the following directions until it hits an edge or visited node
+        # (0,1), (1,0), (0,-1), (-1,0)
+        # directions = [[0,1], [1,0], [0,-1], [-1,0]]
+
+        # i,j= 0,0
+        # ans = []
+        # n = len(matrix)
+        # dirIdx = 0
+        # visited = set()
+
+        # for _ in range(n*n):
+        #     if i<0 or i>=n or j<0 or j>=n or (i,j) in visited
+        #     ans.append(matrix[i][j])
+        #     dirIdx = dirIdx % 4
