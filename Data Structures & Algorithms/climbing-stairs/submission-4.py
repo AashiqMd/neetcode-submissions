857d074f331fa979d03e54a4d59e44f272389912
Aashiq Mohamed Baig <43927911+AashiqMd@users.noreply.github.com>
2026-07-04 03:19:01 -0400

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # dp[i] = number of distinct ways to climb to i
+        dp = [0] * (n+1)
+        for i in range(1,n+1):
+            if i <= 2:
+                dp[i] = i
+            else:
+                dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
