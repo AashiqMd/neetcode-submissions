0b5e75aed15f88d5407c6a7e094c6689dc0fd43e
Aashiq Mohamed Baig <43927911+AashiqMd@users.noreply.github.com>
2026-07-05 16:24:40 -0400

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        parent = [i for i in range(n)]
+        
+        for edge in edges:
+            a,b = edge[0], edge[1]
+            if parent[a] != parent[b]:
+                n-=1
+                temp = parent[b]
+                # parent[b] = parent[a]
+                for i in range(len(parent)):
+                    if parent[i] == temp:
+                        parent[i] = parent[a]
+
+        # print(parent)
+        return n
+
+
+        # for edge in edges:
+        #     a,b = edge[0], edge[1]
+        #     # If at least one of the 2 is getting touched for the 1st time, n-=1
+        #     if not (a in touched and b in touched):
+        #         n-=1
+        #     touched.add(a)
+        #     touched.add(b)
+        # return n
