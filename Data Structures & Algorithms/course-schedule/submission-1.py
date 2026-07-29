060cd1ce939bf67a5a85313f506fecf6bba0c7b6
Aashiq Mohamed Baig <43927911+AashiqMd@users.noreply.github.com>
2026-07-28 23:19:12 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,56 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        visited = set()
+
+        for a,b in prerequisites:
+            adj[b].append(a)
+        
+        def dfs(node):
+            if node in visited:
+                return False
+            visited.add(node)
+            for nei in adj[node]:
+                if not dfs(nei):
+                    return False
+            visited.remove(node)
+            return True
+
+        for node in range(numCourses):
+            if not dfs(node):
+                return False
+        return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # adj = {i : [] for i in range(numCourses)}
+        # visited = set()
+
+        # for pre in prerequisites:
+        #     a,b = pre[0], pre[1]
+        #     adj[a].append(b)
+        
+        # def dfs(node):
+        #     if node in visited:
+        #         return False
+            
+        #     visited.add(node)
+        #     for n in adj[node]:
+        #         if not dfs(n):
+        #             return False
+        #     visited.remove(node)
+        #     adj[node] = []
+        #     return True
+        
+        # for node in range(numCourses):
+        #     if not dfs(node):
+        #         return False
+    
+        # return True
+    
