06f82f8f36034081382c8dbb3ab9eeff65af3b5b
Aashiq Mohamed Baig <43927911+AashiqMd@users.noreply.github.com>
2026-08-09 12:25:39 -0400

    Add: house-robber - submission-10

---
+++ Data Structures & Algorithms/house-robber/submission-10.py
@@ -0,0 +1,44 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # Space optimization for the dp. 
+        # Simpler methid is easier
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        # dp = [0]*(n+2)
+        dp_1, dp_2 = nums[0], max(nums[0],nums[1])
+
+        # for i in range(n-1,-1,-1):
+        #     dp[i] = max(nums[i] + dp[i+2], dp[i+1])
+        
+        for i in range(2,n):
+            dp_1, dp_2 = dp_2, max(nums[i] + dp_1, dp_2)
+        
+        return dp_2
+        
+        # n = len(nums)
+        # if n==1:
+        #     return nums[0]
+        # dp = [-1] * n
+
+        # def dfs(i):
+        #     if i>=n:
+        #         return 0
+        #     if dp[i] != -1:
+        #         return dp[i]
+        #     dp[i] = max(dfs(i+1), nums[i]+dfs(i+2))  
+        #     return dp[i]
+        # return dfs(0)  
+
+        # n = len(nums)
+        # if n==1:
+        #     return nums[0]
+        
+        # dp = [0] * n
+        # dp[n-1], dp[n-2] = nums[n-1], max(nums[n-1], nums[n-2])
+        
+        # for i in range(n-3,-1,-1):
+        #     dp[i] = max(nums[i] + dp[i+2], dp[i+1])
+        
+        # return dp[0]
