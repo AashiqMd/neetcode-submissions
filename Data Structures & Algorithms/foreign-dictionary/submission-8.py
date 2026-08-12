90da7537fee054d8b31990b1cdad8bbac4f77176
Aashiq Mohamed Baig <43927911+AashiqMd@users.noreply.github.com>
2026-08-11 21:25:43 -0400

    Add: foreign-dictionary - submission-8

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-8.py
@@ -0,0 +1,42 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adj = defaultdict(list)
+        indegree = {}
+        uniqueChars = set()
+
+        for word in words:
+            for c in word:
+                if c not in indegree:
+                    indegree[c] = 0
+                    uniqueChars.add(c)
+
+        for i in range(len(words)-1):
+            w1,w2 = words[i], words[i+1]
+            idx = min(len(w1),len(w2))
+
+            if w1[:idx] == w2[:idx] and len(w1) > len(w2):
+                return ""
+            for j in range(idx):
+                if w1[j] != w2[j]: 
+                    adj[w1[j]].append(w2[j])
+                    indegree[w2[j]] += 1
+                    break
+
+        # print(adj)
+        # print(indegree)
+
+        queue = deque()
+        for k in indegree:
+            if indegree[k] == 0:
+                queue.append(k)
+        
+        res = ""
+        while queue:
+            node = queue.popleft()
+            res += node
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    queue.append(nei)
+
+        return res if len(res) == len(uniqueChars) else ""
