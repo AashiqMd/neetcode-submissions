ef17418e8aff262e72e5ee18304c7a9484e55a20
Aashiq Mohamed Baig <43927911+AashiqMd@users.noreply.github.com>
2026-08-12 19:53:15 -0400

    Add: house-robber-ii - submission-6

---
+++ Data Structures & Algorithms/house-robber-ii/submission-6.py
@@ -0,0 +1,34 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # The idea is to use 2 dps. 1st is from House 0 to Hpuse n-1
+        # 2nd DP is for House 1 to House n
+
+        # if len(nums) == 1:
+        #     return nums[0]
+
+        # dp1, dp2 = [0] * len(nums), [0] * len(nums)
+        # dp1[0], dp1[1] = nums[0], max(nums[0], nums[1])
+        # dp2[0], dp2[1] = 0, nums[1]
+
+        # for i in range(2,len(nums)):
+        #     if i != len(nums)-1:
+        #         dp1[i] = max(nums[i] + dp1[i-2], dp1[i-1])
+        #     dp2[i] = max(nums[i] + dp2[i-2], dp2[i-1])
+        
+        # # Since I initialized both dps to the same size, max for dp1 is at dp1[-2]
+        # return max(dp1[-2], dp2[-1])
+
+        if len(nums) == 1:
+            return nums[0]
+        
+        # helper will return the max you can rob in a regular array. 
+        def helper(houses):
+            if len(houses) == 1:
+                return houses[0]
+            
+            dp1, dp2 = houses[0], max(houses[0],houses[1])
+            for i in range(2,len(houses)):
+                dp1, dp2 = dp2, max(houses[i] + dp1, dp2)
+            return dp2
+        
+        return max(helper(nums[:len(nums)-1]), helper(nums[1:]))
